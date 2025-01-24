06157d0424398060a2dbd6797f96e7395fb9b11b
shynur <shynur@outlook.com>
2025-01-25 01:25:36 +0800

    * doc: 更简短的示例.

--- src/ipc-reader.cpp
+++ src/ipc-reader.cpp
@@ -1,12 +1,12 @@
 #include "ipcator.hpp"
 using namespace literals;
 
+ShM_Reader rd;
 int main() {
     std::this_thread::sleep_for(0.3s);  // 等 writer 先创建好消息.
-    const auto descriptor = -"/ipcator.msg_descriptor"_shm;
-    const auto& [name, offset] = (std::pair<std::array<char, 24>, std::size_t>&)descriptor[0];
-
-    ShM_Reader rd;
+    const auto& [name, offset] = rd.template read<
+                                     std::pair<std::array<char, 24>, std::size_t>
+                                 >("/ipcator.msg_descriptor", 0);
     const auto& mul2_add1 = rd.template read<int(int)>(name.data(), offset);
 
     std::this_thread::sleep_for(1.3s);  // 这时 writer 进程已经退出了, 当我们仍能读取消息:
--- src/ipc-writer.cpp
+++ src/ipc-writer.cpp
@@ -1,26 +1,21 @@
 #include "ipcator.hpp"
 using namespace literals;
 
-#include <cstring>
-
-extern "C" int shared_fn(int n) { return n * 2 + 1; }  // 要传递的函数.
-
+int shared_fn(int n) { return n * 2 + 1; }  // 要传递的函数.
 int main() {
-    Monotonic_ShM_Buffer buf;  // 共享内存 buffer.
+    Monotonic_ShM_Buffer /* or ShM_Pool<bool> */ buf;  // 共享内存 buffer.
     const auto block = buf.allocate(0x50);  // 向 buffer 申请内存块.
-    std::memcpy((char *)block, (char *)shared_fn, 0x50);  // 向内存块写入数据.
+    new (block) auto{*(std::array<char, 0x50> *)shared_fn};  // 向内存块写入数据.
 
     // 查找 block 所在的 POSIX shared memory:
     const auto& target_shm = buf.upstream_resource()->find_arena(block);
     // block 在 POSIX shared memory 中的 偏移量:
-    const std::size_t offset = (char *)block - std::data(target_shm);
+    const auto offset = (char *)block - std::data(target_shm);
 
-    // 事先约定的共享内存 vvvvvvvvvvvvvvvv, 用来存放消息的位置区域和偏移量:
+    // 事先约定的共享内存, 用来存放消息的位置区域和偏移量:
     const auto descriptor = "/ipcator.msg_descriptor"_shm[32];
     (std::pair<std::array<char, 24>, std::size_t>&)descriptor[0] = {
-        *(std::array<char, 24> *)target_shm.get_name().c_str(),
-        offset
+        *(std::array<char, 24> *)target_shm.get_name().c_str(), offset
     };
-
     std::this_thread::sleep_for(1s);  // 等待 reader 获取消息.
 }
